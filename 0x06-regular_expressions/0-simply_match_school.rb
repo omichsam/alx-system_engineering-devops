a48fa5e605250b570f366f3adf115919b96f1d82
omichsam <53429860+omichsam@users.noreply.github.com>
2023-10-05 22:36:51 +0300

    Update 0-simply_match_school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+# Check if there is exactly one argument
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <input>"
+  exit 1
+end
+
+# Extract the input from the command-line argument
+input = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /School/
+
+# Use the match method to find the first occurrence of the pattern in the input
+match = input.match(pattern)
+
+# Output the result or an empty string if no match is found
+puts match ? match[0] : ""
