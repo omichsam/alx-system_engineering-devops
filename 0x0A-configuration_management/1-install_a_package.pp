5d614ec483d80ce92a5b94db4e227d9f63138e45
omichsam <omichsam@gmail.com>
2023-12-26 09:55:18 +0300

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,7 @@
-# install a flask app
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
